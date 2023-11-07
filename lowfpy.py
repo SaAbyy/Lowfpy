db13b28e7e91482be91c5d06aa9b5b7ec975b578
SaAbyy <94359910+SaAbyy@users.noreply.github.com>
2023-11-07 14:25:24 +0100

    Update lowfpy.py

--- lowfpy.py
+++ lowfpy.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 print('\
  __                   _____                \n\
@@ -18,3 +19,14 @@ def generate_pseudos(var):
   return 0
 
 dont_break = True
+
+def help():
+  return ("Lowfpy is a tool for generate passwords, wordlists and pseudos. Let's see what you can do \n \
+  Test")
+
+while True :
+  command = sys.stdin.readline()
+  if command == "-h" :
+    output = help()
+    print(output)
+  
