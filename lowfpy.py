6fa973e292bf4526f8a90053a17dd3f3933261b8
SaAbyy <94359910+SaAbyy@users.noreply.github.com>
2023-11-07 14:12:10 +0100

    Create lowfpy.py

---
+++ lowfpy.py
@@ -0,0 +1,20 @@
+import random
+
+print('\
+ __                   _____                \n\
+|  |   ______  _  ___/ ____\_____ ___.__   \n\
+|  |  /  _ \ \/ \/ /\   __/____  <   |  |  \n\
+|  |_(  <_> )     /  |  |  |  |_> >___  |  \n\
+|____/\____/ \/\_/   |__|  |   __// ____|  \n\
+                           |__|   \/     ')
+  
+def wordlists(var):
+  return 0
+
+def generate_passwords(var):
+  return 0
+
+def generate_pseudos(var):
+  return 0
+
+dont_break = True
